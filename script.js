cd9013268c54e5af45897cf22c0fb082cc8dcc9a
Gordon Karlsson <gpkarlsson72@gmail.com>
2022-12-06 10:26:40 -0600

    Added pseudocode, started work on javascript. Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Assignment code here
+// var chars = "0123456789abcdefghijklmnopqrstuvwxyz !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var passwordLength = 12;
+// var password = "";
 
+function generatePassword() {
+  console.log("Button pressed");
+  // 1. Prompt the user for the password criteria
+    // a. Password length 8 < 128
+    // b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input
+// 3. Generate assword based on criteria
+
+// 4. Display password to page
+
+  return "Generated password";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
@@ -15,3 +31,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
