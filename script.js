aad17d75f9e269277da889b4cd83f7bd9b335a77
gordonk <gpkarlsson72@gmail.com>
2022-12-08 15:52:53 -0600

    commented out code that caused an error to troubleshoot another issue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "-", "_", "/", "?", "<", ">", ".", ",", "[", "]", "{", "}", ":", ";", "~", "`"]
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "-", "_", "/", "?", "<", ">", ".", ",", "[", "]", "{", "}", ":", ";", "~", "`"];
 var options = [];
 
 function generatePassword() {
@@ -148,11 +148,11 @@ var password = [];
 
 };
 
-for (var i = 0; i < numChars; i++) {
-  var chooseOptions = options[Math.floor(Math.random() * options.length)];
-  password.push(chooseOptions);
-  console.log(password);
-}
+// for (var i = 0; i < numChars; i++) {
+//   var chooseOptions = options[Math.floor(Math.random() * options.length)];
+//   password.push(chooseOptions);
+//   console.log(password);
+// }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
   
